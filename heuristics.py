01401166f4dcd3e43436351a8abaf3f571921543
ihtis-git <129101723+ihtis-git@users.noreply.github.com>
2024-03-05 17:32:31 +0300

    Update heuristics.py
    Добавлена функция, составляющая список мутаций доменов

--- heuristics.py
+++ heuristics.py
@@ -1,5 +1,10 @@
+import dnstwist
 
 class DomainMutation:
     def __init__(self, domain_list):
         self.domain_list = domain_list
-        self.mutation_file_path = ""
+        self.mutation_file_path = "domain_mutations.txt"
+
+    def get_domain_mutations(self, domain_name):
+        dnstwist.run(domain=domain_name, format='list', 
+                     output = self.mutation_file_path)
