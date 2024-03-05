158d3eb5a15f887f0933a933a9e2e0442c18993d
ihtis-git <fedorlut@yandex.ru>
2024-03-05 18:33:07 +0300

    Update heuristics.py
    Добавлен каркас функции fetch, реализующая эвристику

--- heuristics.py
+++ heuristics.py
@@ -1,10 +1,16 @@
 import dnstwist
 
 class DomainMutation:
-    def __init__(self, domain_list):
-        self.domain_list = domain_list
-        self.mutation_file_path = "domain_mutations.txt"
+    def __init__(self, input_list):
+        self.domain_list = input_list
+        self.mutation_data_filepath = "domain_mutations.txt"
 
     def get_domain_mutations(self, domain_name):
         dnstwist.run(domain=domain_name, format='list', 
-                     output = self.mutation_file_path)
+                     output = self.mutation_data_filepath)
+    
+    #основа функции организации эвристики  
+    def fetch_domain_mutations(self):
+        for domain in self.domain_list:
+            self.get_domain_mutations(domain)
+            #функция, делающая запрос Netlass.io
