d1ec3251d9595ad36723d9ddca615efdac47421c
Ilya Timofeev <53445554+ilya3254@users.noreply.github.com>
2024-03-24 14:04:07 +0000

    Made an exception for legitimate domains. Only for 2 levels, not for 3+ levels.

--- src/phisher/subdomains.py
+++ src/phisher/subdomains.py
@@ -12,21 +12,22 @@ class Subdomains():
     def __init__(self, netlas_connection: Netlas) -> None:
         self.netlas_connection = netlas_connection
         
-    """Generates query like "domain:<name>.* level:[3 TO max_level]"""
+    """Generates query like "domain:<name>.* level:[3 TO max_level] !domain:(*.<legit_domain1> || *.<legit_domain2> || ...)"""
     @staticmethod
-    def _make_query(name: str, max_level: int) -> str:
-        return f"domain:{name}.* level:[3 TO {max_level}]"
+    def _make_query(name: str, legit_domains_exception: str, max_level: int) -> str:
+        return f"domain:{name}.* level:[3 TO {max_level}] " + legit_domains_exception
 
-    def search(self, names: list, max_level: int=4) -> list:
+    def search(self, names: list, legit_domains: list, max_level: int=4) -> list:
         domains = []
+        legit_domains_exception = "!domain:(" + " || ".join([f"*.{domain}" for domain in legit_domains]) + ")"
         for name in names:
-            query = self._make_query(name, max_level)
+            query = self._make_query(name, legit_domains_exception, max_level)
             count = self.netlas_connection.count(datatype="domain",
                                             query=query)['count']
             if count != 0:
                 iterator_of_bytes = self.netlas_connection.download(datatype="domain",
-                                                               query=query,
-                                                               size=count)
+                                                                    query=query,
+                                                                    size=count)
                 # saving domain field in the list by default
                 bytes_data = b"".join(iterator_of_bytes)
                 results = parse_jsons(bytes_data.decode("utf-8"))
@@ -38,7 +39,7 @@ class Subdomains():
 
 # Example for debugging
 if __name__ == "__main__":
-    netlas_connection = Netlas(api_key='apikey')
+    netlas_connection = Netlas(api_key='api_key')
     SubdomainS = Subdomains(netlas_connection)
-    domains = SubdomainS.search(names=["vkontakte"])
+    domains = SubdomainS.search(names=["habr"], legit_domains=["habrastorage.org", "habratest.net"])
     print(domains)
