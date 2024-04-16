5a00cf333820c87cfccaf59a90372c9717c2e1a4
Ilya Timofeev <53445554+ilya3254@users.noreply.github.com>
2024-04-16 15:03:41 +0000

    update main.py

--- src/phisher/__main__.py
+++ src/phisher/__main__.py
@@ -3,46 +3,35 @@ import argparse
 import cout
 import inparse
 import domain_mutations
-
-# Global constants
-is_verbose = False
+import subdomains
+import keywords
 
 
 def main():
-    global is_verbose
-    # Print utility wrapper
-    cout.print_banner()
-
     # Processing command line arguments
     parser = argparse.ArgumentParser()
-
-    parser.add_argument("input_file", help="file with resources")
-    parser.add_argument("api_key", help="personal API key")
-    parser.add_argument("-v", "--verbose", help="run in verbose mode",
-                        action="store_true")
-    parser.add_argument("-n", "--normal", help="run in normal mode",
-                        action="store_true")
-
+    parser.add_argument("-p", "-perimeter", help="file with perimeter data")
+    parser.add_argument("-a", "-apikey", help="personal API key")
     args = parser.parse_args()
 
-    # Select console output mode
-    if args.verbose:
-        is_verbose = True
-        print("Processing in verbose mode...\n")
-    else:
-        is_verbose = False
-        print("Processing in normal mode...")
-
-    percents = 0
     # Processing
-    input_data = inparse.Inparse()
-    input_data.parse(inparse.read(args.input_file))
-    # лучше инициировать подключение с апи ключом сразу, 
-    # а модулям передавать готовый Netlas объект
-    # netlas_connection = netlas.Netlas(api_key=args.api_key)
+    perimeter = inparse.Inparse()
+    perimeter.parse(inparse.read(args.input_file))
+    # модулям передается готовый инициализированный Netlas объект
+    netlas_connection = netlas.Netlas(api_key=args.api_key)
 
-    domain_mutation = domain_mutations.DomainMutations(input_data.domains, args.api_key)
-    domain_mutation.search_mutation_domains(percents)
+    mutations = domain_mutations.DomainMutations(netlas_connection)
+    existing_mutdomains = mutations.search(domains=perimeter.domains)
+    print(existing_mutdomains)
+
+    SubdomainS = subdomains.Subdomains(netlas_connection)
+    existing_subdomains = SubdomainS.search(names=perimeter.brandnames,
+                                            legit_topdomains=perimeter.topdomains)
+    print(existing_subdomains)
+
+    KeywordS = keywords.Keywords(netlas_connection)
+    result = KeywordS.search(domain="www.bspb.ru", keywords=["Банк", "Банк Санкт-Петербург", "БСПБ"])
+    print(result)
 
 
 if __name__ == "__main__":
